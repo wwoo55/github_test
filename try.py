6c5c611f1c435ed57fd2f9b46b3c3d26bff434ab
xy <2297898519@qq.com>
2020-02-04 11:42:55 +0800

    默认的

--- try.py
+++ try.py
@@ -1,5 +1,9 @@
-print("practise")
-print("repeat practise")
-print("end repeat practise")
-print("try refresh")
-print("pull")
+# print("practise")
+# print("repeat practise")
+# print("end repeat practise")
+# print("try refresh")
+# print("pull")
+a=1
+def run (a):
+    return 2*a
+print (run(run(a)))
